88a89c238902c251c872d31d0b28fd983b0e53d5
SHALfEY088 <shalfey088@gmail.com>
2024-02-13 15:45:39 +0300

    added 238. Product of Array Except Self

---
+++ leetcode/easy/238. Product of Array Except Self/main.go
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/product-of-array-except-self/description/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(productExceptSelf([]int{1, 2, 3, 4}))      //[24,12,8,6]
+	fmt.Println(productExceptSelf([]int{-1, 1, 0, -3, 3})) //[0,0,9,0,0]
+}
+
+func productExceptSelf(nums []int) []int {
+	l := len(nums)
+	res := make([]int, l)
+	res[0] = 1
+	for i := 1; i < l; i++ {
+		res[i] = res[i-1] * nums[i-1]
+	}
+
+	rp := 1
+	for i := l - 1; i >= 0; i-- {
+		res[i] = res[i] * rp
+		rp = nums[i] * rp
+	}
+
+	return res
+}
+
+func productExceptSelf_(nums []int) []int {
+	l := len(nums)
+	res := make([]int, l)
+	left := make([]int, l)
+	right := make([]int, l)
+	left[0], right[l-1] = 1, 1
+
+	for l, r := 1, l-2; l < len(nums); l, r = l+1, r-1 {
+		left[l] = left[l-1] * nums[l-1]
+		right[r] = right[r+1] * nums[r+1]
+	}
+
+	for i := 0; i < l; i++ {
+		res[i] = left[i] * right[i]
+	}
+
+	return res
+}
