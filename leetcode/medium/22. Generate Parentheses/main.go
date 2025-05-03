7afd2d00cab6273c978c0c7eac4ccea1a5fae5ad
a.obgemov <a.obgemov@ngrsoftlab.ru>
2025-05-03 10:14:25 +0300

    22. Generate Parentheses

---
+++ leetcode/medium/22. Generate Parentheses/main.go
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/generate-parentheses/description/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(generateParenthesis(3)) // ["((()))","(()())","(())()","()(())","()()()"]
+	fmt.Println(generateParenthesis(1)) // ["()"]
+}
+
+func generateParenthesis(n int) []string {
+
+}
