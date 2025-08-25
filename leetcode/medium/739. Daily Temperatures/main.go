75b008ec6cddce5183f8105251f6447ca9d37564
AndrewHigh08 <samdawsonbai@gmail.com>
2025-08-25 23:20:11 +0300

    739. Daily Temperatures

--- leetcode/medium/739. Daily Temperatures/main.go
+++ leetcode/medium/739. Daily Temperatures/main.go
@@ -1,2 +1,16 @@
 // https://leetcode.com/problems/daily-temperatures/description/
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(dailyTemperatures([]int{73, 74, 75, 71, 69, 72, 76, 73}))
+	fmt.Println(dailyTemperatures([]int{30, 40, 50, 60}))
+	fmt.Println(dailyTemperatures([]int{30, 60, 90}))
+}
+
+func dailyTemperatures(temperatures []int) []int {
+	res := make([]int, 0)
+
+	return res
+}
